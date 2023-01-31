18194338b54c187b500cf67525a4bac892f8882b
Doreapp <doreapp@github.com>
2023-01-31 17:10:58 +0100

    :tada: Introduce python package

---
+++ tools/pyfolio/__init__.py
@@ -0,0 +1,1 @@
+"""Pyfolio python module"""
---
+++ tools/pyfolio/__main__.py
@@ -0,0 +1,10 @@
+"""Main entrypoint"""
+
+
+def main(_cli=None):
+    """Main entrypoint from command line"""
+    print("Hello world")
+
+
+if __name__ == "__main__":
+    main()
