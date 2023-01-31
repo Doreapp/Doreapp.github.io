0bff2327ed65725355bda1611f4acf6fa6e5601f
Doreapp <doreapp@github.com>
2023-01-31 16:20:21 +0100

    :hammer: Fix dev script

--- jekyll/dev.sh
+++ jekyll/dev.sh
@@ -1,2 +1,5 @@
-cp ../me.yml _data/
-bundle exec jekyll serve --host 0.0.0.0
+BASEDIR=$(dirname "$0")
+
+cp $BASEDIR/../me.yml $BASEDIR/_data/
+
+cd $BASEDIR && bundle exec jekyll serve --host 0.0.0.0
