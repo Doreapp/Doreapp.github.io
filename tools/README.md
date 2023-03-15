1678878ea6d58ad8a639a7d0630ac04a72648bc3
Doreapp <doreapp@github.com>
2023-03-15 20:44:03 +0100

    :hammer: Introduce translation python scripts

--- tools/README.md
+++ tools/README.md
@@ -2,8 +2,14 @@
 
 Python module to check the format of `me.yml` file.
 
-## Usage
+## Format check
 
 ```bash
-python3 -m pyfolio path/to/me.yml
+python3 -m pyfolio check path/to/me.yml
+```
+
+## Website translation
+
+```bash
+python3 -m pyfolio translate path/to/source/ path/to/destination/ path/to/override/
 ```
--- tools/pyfolio/__main__.py
+++ tools/pyfolio/__main__.py
@@ -7,7 +7,7 @@ from os import path
 
 import yaml
 
-from . import grammar
+from . import grammar, translator
 
 logging.basicConfig(format="[%(name)s] %(levelname)s: %(message)s")
 LOGGER = logging.getLogger(__name__)
@@ -59,20 +59,57 @@ def exit_based_on_log_level(handler: AggregationHandler, levelname: str):
     sys.exit(1)
 
 
-def main(cli=None):
-    """Main entrypoint from command line"""
-    parser = argparse.ArgumentParser("pyfolio")
-    parser.add_argument("file", help="Path to the file to check", default="me.yml", nargs="?")
-    parser.add_argument(
+def _get_parser():
+    """Build CLI parser"""
+    parser = argparse.ArgumentParser(
+        "pyfolio",
+        description="Program to manage a portfolio",
+        epilog="Built by Antoine MANDIN",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    subparsers = parser.add_subparsers(
+        title="command", description="Program command", dest="command"
+    )
+    check_subparser = subparsers.add_parser(
+        "check",
+        help="Check the format of a yml file",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    check_subparser.add_argument(
+        "file", help="Path to the file to check", default="me.yml", nargs="?"
+    )
+    check_subparser.add_argument(
         "--fail-at",
         help="Minimum error level to return a non null exit code (NONE to never fail)",
         default="NONE",
         choices=("NONE", "INFO", "WARNING", "ERROR"),
     )
+    translate_subparser = subparsers.add_parser(
+        "translate",
+        help="Translate a jekyll source folder, overriding some parts of the configuration",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    translate_subparser.add_argument("source", help="Path to jekyll source directory")
+    translate_subparser.add_argument("dest", help="Path to destination directory")
+    translate_subparser.add_argument("override", help="Path to overriding configuration directory")
+    translate_subparser.add_argument(
+        "--force", action="store_true", help="Whether to overriding existing destination directory"
+    )
+    return parser
+
+
+def main(cli=None):
+    """Main entrypoint from command line"""
+    parser = _get_parser()
     args = parser.parse_args(cli)
-    handler = AggregationHandler()
-    check_file(get_grammar(), args.file, handler)
-    exit_based_on_log_level(handler, args.fail_at)
+    if args.command == "check":
+        handler = AggregationHandler()
+        check_file(get_grammar(), args.file, handler)
+        exit_based_on_log_level(handler, args.fail_at)
+    elif args.command == "translate":
+        translator.translate(args.source, args.dest, args.override, args.force)
+    else:
+        raise argparse.ArgumentError("command", f"Unknown command '{args.command}'")
 
 
 if __name__ == "__main__":
